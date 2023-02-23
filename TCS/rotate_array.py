39b9b86d3c9e9bba9c518b1b0240de1643f3e2bb
Rohith C Dinesan <56485934+rohithcd@users.noreply.github.com>
2023-02-23 23:18:51 +0530

    updated rotate_Array file

--- TCS/rotate_array.py
+++ TCS/rotate_array.py
@@ -1,1 +1,30 @@
-"py" 
+def solution(arr, n, k):
+    result = []
+    
+    # If rotation is greater
+    # than size of array
+    k = k % n
+ 
+    for i in range(n):
+ 
+        if(i < k):
+
+            # Printing rightmost
+            # kth elements
+            result.append(arr[n + i - k])
+ 
+        else:
+ 
+            # Prints array after
+            # 'k' elements
+            result.append(arr[i - k])
+ 
+    return result
+ 
+# Driver code
+#Example
+Array = [ 1, 2, 3, 4, 5 ]
+N = len(Array)
+K = 5
+     
+print(solution(Array, N, K))
